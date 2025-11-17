6f650cae1092052797462c9f77dbe387b9ac0a77
arg50 <arg50@LD>
2025-11-17 21:20:20 +0200

    Revert "new 1"
    This reverts commit 649df1e1f67063bed522d9cd5c5b1ebd4056eaa3.

--- Program.cs
+++ Program.cs
@@ -2,7 +2,6 @@
 {
     class Student
     {
-        int phonenumber;
         string? name;
         string? secondname;
         string? father;
@@ -13,10 +12,20 @@
         List<int> homeworks = new List<int>();
         List<int> lessons = new List<int>();
 
-        public void SetName(string name) { this.name = name; }
-        public void SetSecondName(string secondname) { this.secondname = secondname; }
-        public void SetFather(string father) { this.father = father; }
+        int number;
+        public void SetName(string name)
+        {
+            this.name = name;
+        }
 
+        public void SetSecondName(string secondname)
+        {
+            this.secondname = secondname;
+        }
+        public void SetFather(string father)
+        {
+            this.father = father;
+        }
         public void SetBirthday(int day, int month, int year)
         {
             birthday.Clear();
@@ -24,23 +33,40 @@
             birthday.Add(month);
             birthday.Add(year);
         }
-
         public void SetAddress(string street, string house)
         {
             birthday.Clear();
             address.Add(street);
             address.Add(house);
         }
-
-        public void SetNumber(int number) { this.phonenumber = number; }
-        public void SetExam(int exam) { exams.Add(exam); }
-        public void SetHomework(int homework) { homeworks.Add(homework); }
-        public void SetLesson(int lesson) { lessons.Add(lesson); }
-
-        public string GetName() { return name!; }
-        public string GetSecondName() { return secondname!; }
-        public string GetFather() { return father!; }
-
+        public void SetNumber(int number)
+        {
+            this.number = number;
+        }
+        public void SetExam(int exam)
+        {
+            exams.Add(exam);
+        }
+        public void SetHomework(int homework)
+        {
+            homeworks.Add(homework);
+        }
+        public void SetLesson(int lesson)
+        {
+            lessons.Add(lesson);
+        }
+        public string GetName()
+        {
+            return name!;
+        }
+        public string GetSecondName()
+        {
+            return secondname!;
+        }
+        public string GetFather()
+        {
+            return father!;
+        }
         public int GetBirthday()
         {
             foreach (int number in birthday)
@@ -49,7 +75,6 @@
             }
             return 0;
         }
-
         public string GetAddress()
         {
             foreach (string number in address)
@@ -58,7 +83,6 @@
             }
             return "";
         }
-
         public int GetExam()
         {
             foreach (int number in exams)
@@ -67,7 +91,6 @@
             }
             return 0;
         }
-
         public int GetHomework()
         {
             foreach (int number in homeworks)
@@ -76,7 +99,6 @@
             }
             return 0;
         }
-
         public int GetLesson()
         {
             foreach (int number in lessons)
@@ -85,12 +107,11 @@
             }
             return 0;
         }
-
-        public int GetNumber() { return phonenumber; }
-
-        public Student(string name, string secondname, string father,
-                       int day, int month, int year,
-                       string street, string house, int number)
+        public int GetNumber()
+        {
+            return number;
+        }
+        public Student(string name, string secondname, string father, int day, int month, int year, string street, string house, int number)
         {
             SetName(name);
             SetSecondName(secondname);
@@ -99,96 +120,26 @@
             SetAddress(street, house);
             SetNumber(number);
         }
-
-        public Student(int count_lesson, int count_homework, int count_exam,
-                       int lesson, int exam, int homework)
+        public Student(int count_lesson, int count_homework, int count_exam, int lesson, int exam, int homework)
         {
-            for (int i = 0; i < count_lesson; i++) { SetLesson(lesson); }
-            for (int i = 0; i < count_homework; i++) { SetHomework(homework); }
-            for (int i = 0; i < count_exam; i++) { SetExam(exam); }
-        }
-
-        public Student()
-            : this("NoName", "NoSecondName", "NoFather",
-                   1, 1, 2000, "NoStreet", "NoHouse", 0)
-        { }
-    }
-
-    class Group
-    {
-        List<Student> students;
-        string groupName;
-        string specialization;
-        int course;
-
-        public Group()
-        {
-            students = new List<Student>();
-            groupName = "P45";
-            specialization = ".NET";
-            course = 1;
-        }
-
-        public Group(List<Student> students)
-        {
-            this.students = new List<Student>(students);
-            groupName = "NoName";
-            specialization = "None";
-            course = 1;
-        }
-
-        public Group(Group group)
-        {
-            this.groupName = group.groupName;
-            this.specialization = group.specialization;
-            this.course = group.course;
-            this.students = new List<Student>(group.students);
-        }
-
-        public void ShowGroup()
-        {
-            Console.WriteLine($"Group: {groupName}, Specialization: {specialization}, Course: {course}");
-            Console.WriteLine("Students:");
-
-            var sorted = students
-                .OrderBy(s => s.GetSecondName())
-                .ThenBy(s => s.GetName())
-                .ToList();
-
-            int i = 1;
-            foreach (var student in sorted)
+            for (int i = 0; i < count_lesson; i++)
             {
-                Console.WriteLine($"{i}. {student.GetSecondName()} {student.GetName()}");
-                i++;
+                SetLesson(lesson);
+            }
+            for (int i = 0; i < count_homework; i++)
+            {
+                SetHomework(homework);
+            }
+            for (int i = 0; i < count_exam; i++)
+            {
+                SetExam(exam);
             }
         }
-
-        public void AddStudent(Student student)
+        public Student() : this("NoName", "NoSecondName", "NoFather", 1, 1, 2000, "NoStreet", "NoHouse", 0)
         {
-            students.Add(student);
-        }
 
-        public void TransferStudent(Group otherGroup, Student student)
-        {
-            if (students.Contains(student))
-            {
-                students.Remove(student);
-                otherGroup.students.Add(student);
-            }
-        }
-
-        public void ExpelAllFailed()
-        {
-            students.RemoveAll(s => s.GetExam() < 60);
-        }
-
-        public void ExpelWorst()
-        {
-            if (students.Count == 0) return;
-
-            var worst = students.OrderBy(s => s.GetExam()).First();
-            students.Remove(worst);
-        }
+        } 
+        
     }
     internal class Program
     {
@@ -197,18 +148,9 @@
             Student student1 = new Student("John", "Doe", "Smith", 15, 6, 2002, "Main St", "123A", 1);
             Student student2 = new Student(5, 10, 3, 1, 90, 85);
             Student student3 = new Student();
-
             Console.WriteLine($"Student 1: {student1.GetName()} {student1.GetSecondName()}, Father: {student1.GetFather()}");
             Console.WriteLine($"Student 2 Exam Score: {student2.GetExam()}");
             Console.WriteLine($"Student 3 Name: {student3.GetName()}");
-
-            Group g1 = new Group();
-            g1.AddStudent(student1);
-            g1.AddStudent(student2);
-            g1.AddStudent(student3);
-
-            Console.WriteLine("\nGroup info:");
-            g1.ShowGroup();
         }
     }
 }
